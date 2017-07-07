d1f301970f6d117cb2edd2502ea5f3935e6a3642
Prasanna Santhanam <tsp@apache.org>
2017-07-07 12:09:21 +0530

    fixes #19 prompt_toolkit 1.0.10 minimum required

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if version.major == 2:
 
 
 requires = [
-    'prompt-toolkit>=1.0.0,<1.1.0',
+    'prompt-toolkit>=1.0.10,<1.1.0',
     'Pygments>=2.1.3,<3.0.0',
     'fuzzyfinder>=1.0.0',
     'click>=4.0,<7.0',
